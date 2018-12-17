f0c4bd58c22909a2cb4826b9a5d2df25d2ff5082
Tahnee Evans <ramblor@users.noreply.github.com>
2018-12-17 11:22:56 +0000

    Update xray.js

--- xray.js
+++ xray.js
@@ -101,6 +101,12 @@ $(document).ready(function() {
 
           });
       }
+    
+    function getWidth() {
+        var w = $('#xray-container').width();
+        $('#w').text(w);
+    }
   
     executeMorningstarLoader();
+    getWidth();
   });
